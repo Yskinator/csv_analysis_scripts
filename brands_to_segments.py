05f900d85654db47dbc167ce2aff4e0da2568951
MagusDei <jami.valorinta@helsinki.fi>
2020-05-22 15:54:03 +0300

    Added brands_to_segments, refactored embedding_match

---
+++ brands_to_segments.py
@@ -0,0 +1,67 @@
+"""Connect brands to their most likely product segments."""
+
+import csv
+import spacy
+from embedding_match import extract, load_segments
+
+def load_brands(filepath, brandcounts):
+    """Load brands from csv specified by filepath, while ignoring brands with a brandcount < 20."""
+    with open(filepath, encoding="UTF-8") as f:
+        r = csv.DictReader(f)
+        brands = {}
+        for row in r:
+            brand_str = row['Brands']
+            descr = row['Description']
+            for brand in set(brand_str.split(";")):
+                if not brand:
+                    continue
+                if brand in brandcounts and int(brandcounts[brand]) < 20:
+                    # Ignore small brands, the results would not be reliable
+                    continue
+                if brand in brands:
+                    brands[brand].append(descr)
+                else:
+                    brands[brand] = [descr]
+    return brands
+
+def match_brands_segments(nlp, brands, brandcounts, commodities, segments):
+    """Match brands to most probable segments."""
+    brands_to_segments = [['Brand', 'Count', 'Segment Name']]
+    for i, brand in enumerate(brands.keys()):
+        # Run embedding matching on descriptions
+        possible_segments = []
+        for descr in brands[brand]:
+            descr_doc = nlp(descr)
+            if descr_doc.has_vector and descr_doc.vector_norm != 0:
+                results = extract(descr_doc, commodities, 1)
+                possible_segments.append(segments[results[0][0]])
+        segment = max(possible_segments, key=possible_segments.count)
+        count = brandcounts[brand] if brand in brandcounts else 0
+        brands_to_segments.append([brand, count, '"'+segment+'"'])
+        if i%100 == 0:
+            print(i, (brand, segment))
+    return brands_to_segments
+
+def main():
+    print("Loading brand counts...")
+    with open("brand_counts.csv", encoding="UTF-8") as f:
+        r = csv.DictReader(f)
+        brandcounts = {}
+        for row in r:
+            brandcounts[row['Brand']] = row['Count']
+    print("Loading brands...")
+    brands = load_brands("preprocessed_stocks_with_brands.csv", brandcounts)
+    print("Loading spacy vectors...")
+    nlp = spacy.load("en_vectors_web_lg")
+    nlp.max_length = 1006000
+    print("Loading segments...")
+    commodities, segments = load_segments('segment_strings.csv', nlp)
+    print("Matching brands to segments...")
+    brands_to_segments = match_brands_segments(nlp, brands, brandcounts, commodities, segments)
+    print("Writing to file...")
+    with open('brands_to_segments.csv', encoding="UTF-8", mode="w+") as f:
+        f.write("\n".join([",".join(r) for r in brands_to_segments]))
+    print("Finished.")
+
+if __name__ == "__main__":
+    main()
--- embedding_match.py
+++ embedding_match.py
@@ -4,20 +4,9 @@ import spacy
 
 csv.field_size_limit(int(sys.maxsize/100000000000))
 
-def extract(text, choices, num):
-    ordered_choices = []
-    for i, choice in enumerate(choices):
-        ordered_choices.append((i, choice.similarity(text)))
-    ordered_choices = sorted(ordered_choices, key=lambda row: row[1], reverse=True)
-    return ordered_choices[0:num]
-
-if __name__ == "__main__":
-    print("Loading spacy vectors...")
-    nlp = spacy.load("en_vectors_web_lg")
-    nlp.max_length = 1006000
-    print("Reading commodity types...")
-    #with open('unspsc_codes_3.csv') as f:
-    with open('segment_strings.csv') as f:
+def load_segments(segments_file, nlp):
+    """Load segments from file segments_file."""
+    with open(segments_file) as f:
         r = csv.DictReader(f)
         commodities = []
         segments = []
@@ -27,19 +16,48 @@ if __name__ == "__main__":
             segment = row['Segment Name']
             commodities.append(nlp(commodity))
             segments.append(segment)
-    print("Extracting...")
-    with open('stock_sample_preprocessed.csv', encoding="UTF-8") as f:
+    return (commodities, segments)
+
+def extract(text, choices, num):
+    ordered_choices = []
+    for i, choice in enumerate(choices):
+        ordered_choices.append((i, choice.similarity(text)))
+    ordered_choices = sorted(ordered_choices, key=lambda row: row[1], reverse=True)
+    return ordered_choices[0:num]
+
+def add_segments(filepath, nlp, commodities, segments):
+    """Read rows from supplied csv filepath, calculate and append segments string as new column."""
+    with open(filepath, encoding="UTF-8") as f:
         r = csv.reader(f)
         new_rows = []
         for i, row in enumerate(r):
             new_row = row.copy()
-            descr = row[0]#.split(";")[0]
+            descr = row[0]
             results = extract(nlp(descr), commodities, 3)
-            #result_segment = segments[result_index]
             result_segments = [(segments[i], prob) for i, prob in results]
             if i%10 == 0:
                 print(descr+" -> "+str(result_segments))
             new_rows.append(new_row+[";".join([r[0] for r in result_segments])])
-    with open('stock_sample_with_segments.csv', mode="w+") as sample_file:
-        sample_file.write("\n".join([",".join(row) for row in new_rows]))
+        new_rows[0][2] = "Segments" # Fix the third column of header prior to writing to file
+    return new_rows
+
+def csv_write(filepath, rows):
+    with open(filepath, encoding="UTF-8", mode="w+", newline='') as sample_file:
+        writer = csv.writer(sample_file)
+        for row in rows:
+            writer.writerow(row)
+
+def main():
+    print("Loading spacy vectors...")
+    nlp = spacy.load("en_vectors_web_lg")
+    nlp.max_length = 1006000
+    print("Reading commodity types...")
+    commodities, segments = load_segments('segment_strings.csv', nlp)
+    print("Extracting...")
+    new_rows = add_segments('stock_sample_preprocessed.csv', nlp, commodities, segments)
+    print("Writing to file...")
+    csv_write('stock_sample_with_segments.csv', new_rows)
     print("Finished.")
+
+if __name__ == "__main__":
+    main()
