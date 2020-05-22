7f3317679d2be072dffaeedde88e5f67278eb17f
MagusDei <jami.valorinta@helsinki.fi>
2020-05-22 16:31:30 +0300

    Added brand segment matching to embedding_match

--- embedding_match.py
+++ embedding_match.py
@@ -18,6 +18,15 @@ def load_segments(segments_file, nlp):
             segments.append(segment)
     return (commodities, segments)
 
+def load_brand_segments(filepath):
+    """Load segments associated with a file."""
+    with open(filepath, encoding="UTF-8") as f:
+        r = csv.DictReader(f)
+        brand_seg = {}
+        for row in r:
+            brand_seg[row['Brand']] = row['Segment Name']
+    return brand_seg
+
 def extract(text, choices, num):
     ordered_choices = []
     for i, choice in enumerate(choices):
@@ -25,7 +34,7 @@ def extract(text, choices, num):
     ordered_choices = sorted(ordered_choices, key=lambda row: row[1], reverse=True)
     return ordered_choices[0:num]
 
-def add_segments(filepath, nlp, commodities, segments):
+def add_segments(filepath, nlp, commodities, segments, brand_seg=None):
     """Read rows from supplied csv filepath, calculate and append segments string as new column."""
     with open(filepath, encoding="UTF-8") as f:
         r = csv.reader(f)
@@ -33,9 +42,15 @@ def add_segments(filepath, nlp, commodities, segments):
         for i, row in enumerate(r):
             new_row = row.copy()
             descr = row[0]
+            brand = row[2]
             results = extract(nlp(descr), commodities, 3)
             result_segments = [(segments[i], prob) for i, prob in results]
-            if i%10 == 0:
+            if brand_seg and brand in brand_seg.keys():
+                segment = brand_seg[brand]
+                if not segment in result_segments:
+                    result_segments = [(segment, 1.0)]+result_segments
+                    print("Row "+str(i)+": Added segment "+segment+" to "+descr+" based on brand.")
+            if i%250 == 0:
                 print(descr+" -> "+str(result_segments))
             new_rows.append(new_row+[";".join([r[0] for r in result_segments])])
         new_rows[0][2] = "Segments" # Fix the third column of header prior to writing to file
@@ -53,8 +68,10 @@ def main():
     nlp.max_length = 1006000
     print("Reading commodity types...")
     commodities, segments = load_segments('segment_strings.csv', nlp)
+    print("Loading brand segments...")
+    brand_seg = load_brand_segments("brands_to_segments.csv")
     print("Extracting...")
-    new_rows = add_segments('stock_sample_preprocessed.csv', nlp, commodities, segments)
+    new_rows = add_segments('preprocessed_stocks_with_brands.csv', nlp, commodities, segments, brand_seg)
     print("Writing to file...")
     csv_write('stock_sample_with_segments.csv', new_rows)
     print("Finished.")
