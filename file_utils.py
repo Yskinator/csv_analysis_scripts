54f8c9878b4088e249136d6058bfde3c8bd45b09
Yskinator <yski2moreletters@gmail.com>
2020-05-26 21:05:08 +0300

    Save_csv fieldnames is now an empty list by default, since it's supposed to be a list of strings.

--- file_utils.py
+++ file_utils.py
@@ -1,12 +1,12 @@
 import os
 import csv
 
-def save_csv(filename, rows, mode = "w", fieldnames = ""):
+def save_csv(filename, rows, mode = "w", fieldnames = []):
     exists = False
     if os.path.isfile(filename) and mode == "a":
        exists = True
     with open(filename, mode) as  fo:
-        if fieldnames == "":
+        if fieldnames == []:
             w = csv.DictWriter(fo, fieldnames = list(rows[0].keys()))
         else:
             w = csv.DictWriter(fo, fieldnames = fieldnames)
