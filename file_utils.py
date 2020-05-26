1fb1513bdd2debead29782f48c06611d2b781ef8
Yskinator <yski2moreletters@gmail.com>
2020-05-26 21:00:25 +0300

    Added a utility file with functions for reading and writing csv.

---
+++ file_utils.py
@@ -0,0 +1,25 @@
+import os
+import csv
+
+def save_csv(filename, rows, mode = "w", fieldnames = ""):
+    exists = False
+    if os.path.isfile(filename) and mode == "a":
+       exists = True
+    with open(filename, mode) as  fo:
+        if fieldnames == "":
+            w = csv.DictWriter(fo, fieldnames = list(rows[0].keys()))
+        else:
+            w = csv.DictWriter(fo, fieldnames = fieldnames)
+        if not exists:
+            w.writeheader()
+        for r in rows:
+            w.writerow(r)
+
+
+def read_file(filename, rows):
+    rows = []
+    with open(filename) as f:
+        r = csv.DictReader(f)
+        for row in r:
+            rows.append(row)
+    return rows
