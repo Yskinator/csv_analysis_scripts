53dd68bcd277b451ec8de1bceb0846e5b3607278
MagusDei <jami.valorinta@helsinki.fi>
2020-06-02 17:07:02 +0300

    Fixed imports, equality checking in regression_tests

--- regression_tests.py
+++ regression_tests.py
@@ -1,9 +1,9 @@
 """Regression tests for analysis scripts."""
 
 import unittest
-import file_utils
+#from utilities import read_csv
 import csv_scripts
-#from .file_utils import read_csv
+from file_utils import read_csv
 #from . import csv_scripts
 import sys
 
@@ -13,12 +13,11 @@ class RegressionTestCase(unittest.TestCase):
     def test_regression(self):
         """Rerun code on regression_test_stock_master.csv and make sure the results match."""
         #print(sys.path)
-        stock_master = file_utils.read_csv("regression_test_stock_master.csv")
-        rows, fieldnames = csv_scripts.add_commodities_to_stocks(stock_master[:250])
+        stock_master = read_csv("regression_test_stock_master.csv")
+        rows, fieldnames = csv_scripts.add_commodities_to_stocks(stock_master[:100])
         all_matched = True
         for i, row in enumerate(rows):
-            #print(stock_master[i])
-            if not row == stock_master[i]:
+            if not row['Commodity'] == stock_master[i]['Commodity']:
                 print("Did not match, row "+str(i)+", "+row['text']+": "+row['Commodity']+" vs. "+stock_master[i]['Commodity'])
                 all_matched = False
         assert(all_matched)
