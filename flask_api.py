a82e8bbb7d9333e87d9152cc044bfa95a2a35373
Yskinator <yski2moreletters@gmail.com>
2020-06-02 18:41:36 +0300

    Made the heroku return single row queries immediately now that we're fast.

--- flask_api.py
+++ flask_api.py
@@ -21,6 +21,9 @@ def api():
         brand = request.args.get("brand", default="")
         text = request.args.get("description")
         rows = [{"Brand": brand, "id": "1", "text": text}]
+        results = csv_scripts.add_commodities_to_stocks(rows)
+        msg = "Success!"
+        return {"Results": result[0], "Errors": [], "Status": status, "Message": msg}
     else:
         data = request.json
         rows = []
@@ -30,26 +33,26 @@ def api():
             else:
                 brand = ""
             rows.append({"Brand": brand, "id": str(i), "text": row["description"]})
-    row_hash = str(hash(json.dumps(rows)))
-    try:
-        j = Job.fetch(row_hash, conn)
-        status = j.get_status()
-        if status == "finished":
-            if isinstance(j.result, Exception):
-                print(j.result)
-                msg = "Something went wrong and now everything is on fire."
-                return {"Results": [], "Errors": [str(j.result)], "Status": status, "Message": msg}
+        row_hash = str(hash(json.dumps(rows)))
+        try:
+            j = Job.fetch(row_hash, conn)
+            status = j.get_status()
+            if status == "finished":
+                if isinstance(j.result, Exception):
+                    print(j.result)
+                    msg = "Something went wrong and now everything is on fire."
+                    return {"Results": [], "Errors": [str(j.result)], "Status": status, "Message": msg}
+                else:
+                    msg = "Success!"
+                    return {"Results": j.result[0], "Errors": [], "Status": status, "Message": msg}
             else:
-                msg = "Success!"
-                return {"Results": j.result[0], "Errors": [], "Status": status, "Message": msg}
-        else:
-            msg = "Try again later."
-            if status == "failed":
-                msg = "Time limit exceeded. Try a smaller job."
-            return {"Results": [], "Errors": [], "Status": status, "Message": msg}
+                msg = "Processing. Ask again in a few minutes to see your results."
+                if status == "failed":
+                    msg = "Time limit exceeded. Try a smaller job."
+                return {"Results": [], "Errors": [], "Status": status, "Message": msg}
 
-    except NoSuchJobError:
-        j = q.enqueue(csv_scripts.add_commodities_to_stocks, rows, job_id=row_hash)
-        msg = "Processing. Ask again in a few minutes to see your results."
-        return {"Results": [], "Errors": [], "Status": j.get_status(), "Message": msg}
+        except NoSuchJobError:
+            j = q.enqueue(csv_scripts.add_commodities_to_stocks, rows, job_id=row_hash)
+            msg = "Processing. Ask again in a few minutes to see your results."
+            return {"Results": [], "Errors": [], "Status": j.get_status(), "Message": msg}
 
